81a7e73df03f4fdb1a274d98f846dddf9aada681
Tofig Ganiyev <143221445+Tofigkh1@users.noreply.github.com>
2025-02-09 18:21:45 +0400

    Delete python-inheritance/2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Module for is_same_class function.
-"""
-
-
-def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of """
-    return type(obj) is a_class
