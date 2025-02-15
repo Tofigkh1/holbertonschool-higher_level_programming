fa1a4414633d296f10860f8fbd10c61336d23056
Tofig Ganiyev <143221445+Tofigkh1@users.noreply.github.com>
2025-02-15 23:07:56 +0400

    Add files via upload

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-import json
+"""
+basic serialization
+"""
+import json as js
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file)
+    """
+    dump
+    """
+
+    with open(filename, 'w') as file:
+        data_js = js.dump(data, file)
+
+
+"""
+basic deserialization
+"""
+
 
 def load_and_deserialize(filename):
-    with open(filename, "r") as dict_file:
-        return json.load(dict_file)
+    """
+    load
+    """
+
+    with open(filename) as file:
+        data_js = js.load(file)
+        return (data_js)
